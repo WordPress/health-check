f0950a4855b698ccc1f4882a27758c84dc3858a2
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 10:58:57 +0000

    First pass at a JSON availablity check.
    git-svn-id: https://plugins.svn.wordpress.org/health-check/trunk@405333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- health-check.php
+++ health-check.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://wordpress.org/extend/plugins/health-check/
 	Description: Checks the health of your WordPress install
 	Author: The WordPress.org community
-	Version: 0.2.1
+	Version: 0.3-alpha
 	Author URI: http://wordpress.org/extend/plugins/health-check/
  */
 define('HEALTH_CHECK_PHP_VERSION', '5.2.4');
@@ -15,6 +15,8 @@ class HealthCheck {
 	function action_plugins_loaded() {
 		if ( get_transient( 'health-check-version-test' ) )
 			add_action('admin_notices', array('HealthCheck', 'action_admin_notice'));
+		
+		add_action( 'admin_menu', array( 'HealthCheck', 'action_admin_menu' ) );
 	}
 
 	function action_activate() {
@@ -22,9 +24,28 @@ class HealthCheck {
 	}
 
 	function action_admin_notice() {
+		echo HealthCheck::get_message();
+		delete_transient( 'health-check-version-test' );
+	}
+
+	function action_admin_menu() {
+		add_dashboard_page( __( 'Heath Check', 'health-check' ), __( 'Heath Check', 'health-check' ), 'manage_options', 'health-check', array( 'HealthCheck', 'dashboard_page' ) );
+	}
+	
+	function dashboard_page() {
+		?>
+		<div class="wrap"><?php screen_icon(); ?>
+			<h2><?php _e( 'Heath Check', 'health-check' ); ?></h2>
+			<?php echo HealthCheck::get_message(); ?>
+		</div>
+		<?php
+	}
+
+	function get_message() {
 		global $wpdb;
 		$php_version_check = version_compare(HEALTH_CHECK_PHP_VERSION, PHP_VERSION, '<=');
 		$mysql_version_check = version_compare(HEALTH_CHECK_MYSQL_VERSION, $wpdb->db_version(), '<=');
+		$json_check = HealthCheck::json_check();
 		$db_dropin = file_exists( WP_CONTENT_DIR . '/db.php' );
 		$message ='';
 		
@@ -37,18 +58,29 @@ class HealthCheck {
 				$message .= "<p><strong>".__('Note:', 'health-check')."</strong> ".__('You are using a <code>wp-content/db.php</code> drop-in which may not being using a MySQL database.', 'health-check')."</p>";
 		}
 
-		if ( $php_version_check && $mysql_version_check ) {
+		if ( ! $json_check ) {
+			$message .= "<p><strong>".__('Note:', 'health-check')."</strong> ".__('The PHP install on your server has JSON disabled and is therefore not compatible with WordPress 3.2.', 'health-check')."</p>";
+		}
+
+		if ( $php_version_check && $mysql_version_check && $json_check) {
 			$message .= "<p><strong>".__('Excellent:', 'health-check')."</strong> ".sprintf(__('Your server is running PHP version %1$s and MySQL version %2$s which will be great for WordPress 3.2 onward.', 'health-check'), PHP_VERSION, $wpdb->db_version())."</p>";
 			$message = "<div id='health-check-warning' class='updated'>" . $message . "</div>";
 		} else {
-			$message .= "<p>".__('Once your host has upgraded your server you can re-activate the plugin to check again.', 'health-check')."</p>";
+			$message .= "<p>". sprintf( __('Once your host has upgraded your server you can visit <a href=%s>Dashboard > Health Check</a> to check your compatibility again.', 'health-check' ), menu_page_url( 'health-check', false ) ) ."</p>";
 			$message = "<div id='health-check-warning' class='error'>" . $message . "</div>";
 		}
-		echo $message;
+		
 
-		delete_transient( 'health-check-version-test' );
+		return $message;
 	}
+	
+	function json_check() {
+		$extension_loaded = extension_loaded( 'json' );
+		$functions_exist = function_exists( 'json_encode' ) && function_exists( 'json_decode' );
+		$functions_work = function_exists( 'json_encode' ) && ( '' != json_encode( 'my test string' ) );
 
+		return $extension_loaded && $functions_exist && $functions_work;
+	}
 }
 /* Initialize ourselves */
 add_action('plugins_loaded', array('HealthCheck','action_plugins_loaded'));
