2469c26a36448b53efda300637d7a790dfab82f2
Xenos Konstantinos <mr.xkon@gmail.com>
2018-02-19 12:36:55 +0200

    check for newer plugin versions
    Introducing a check for newer plugin versions

--- includes/class-health-check-debug-data.php
+++ includes/class-health-check-debug-data.php
@@ -500,7 +500,8 @@ class Health_Check_Debug_Data {
 		}
 
 		// List all available plugins.
-		$plugins = get_plugins();
+		$plugins        = get_plugins();
+		$plugin_updates = get_plugin_updates();
 
 		foreach ( $plugins as $plugin_path => $plugin ) {
 			$plugin_part = ( is_plugin_active( $plugin_path ) ) ? 'wp-plugins-active' : 'wp-plugins-inactive';
@@ -523,9 +524,16 @@ class Health_Check_Debug_Data {
 				$plugin_version_string = sprintf( __( 'Version %s', 'health-check' ), $plugin_version );
 			}
 
+			if ( array_key_exists( $plugin_path, $plugin_updates ) ) {
+				// translators: %s: Latest plugin version number.
+				$update_needed = ' ' . sprintf( __( '( Latest version: %s )', 'health-check' ), $plugin_updates[ $plugin_path ]->update->new_version );
+			} else {
+				$update_needed = '';
+			}
+
 			$info[ $plugin_part ]['fields'][] = array(
 				'label' => $plugin['Name'],
-				'value' => $plugin_version_string,
+				'value' => $plugin_version_string . $update_needed,
 			);
 		}
 
