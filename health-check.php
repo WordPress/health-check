1a15fec943e167b94b78d852487641e2a6b1a9e7
Xenos Konstantinos <mr.xkon@gmail.com>
2018-02-21 18:42:32 +0200

    fixing mail check action
    fixing mail check action

--- health-check.php
+++ health-check.php
@@ -101,7 +101,7 @@ class HealthCheck {
 		add_action( 'wp_ajax_health-check-loopback-individual-plugins', array( 'Health_Check_Loopback', 'loopback_test_individual_plugins' ) );
 		add_action( 'wp_ajax_health-check-files-integrity-check', array( 'Health_Check_Files_Integrity', 'run_files_integrity_check' ) );
 		add_action( 'wp_ajax_health-check-view-file-diff', array( 'Health_Check_Files_Integrity', 'view_file_diff' ) );
-		add_action( 'wp_ajax_health-check-mail-check', array( 'Mail_Check', 'run_mail_check' ) );
+		add_action( 'wp_ajax_health-check-mail-check', array( 'Health_Check_Mail_Check', 'run_mail_check' ) );
 		add_action( 'wp_ajax_health-check-confirm-warning', array( 'Health_Check_Troubleshoot', 'confirm_warning' ) );
 	}
 
