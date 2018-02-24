1a55ccb87fb0b806acdd0a7a5961ac5b6e760c92
Clorith <bg3erl42>
2018-02-24 22:12:29 +0100

    Split up our tests so they run in different function calls and failures can be traced.

--- tests/phpunit/test-mu-plugin.php
+++ tests/phpunit/test-mu-plugin.php
@@ -4,37 +4,33 @@ use PHPUnit\Framework\TestCase;
 
 class AssertionTest extends TestCase {
 
-	public function testHasDefaultTheme() {
-		$mu_plugin = new Health_Check_Troubleshooting_MU();
+	public function setUp() {
+		parent::setUp();
 
-		// Troubleshooting mode should be disabled by default.
-		$this->assertFalse( $mu_plugin->is_troubleshooting() );
+		$this->class_instance = new Health_Check_Troubleshooting_MU();
 
 		// Set up a Troubleshooting hash to test if it is enabled.
 		update_option( 'health-check-disable-plugin-hash', 'abc123' );
+	}
 
+	public function testTroubleshootingModeDisabledNoCookie() {
+		// Troubleshooting mode should be disabled by default, with no cookie declared.
+		$this->assertFalse( $this->class_instance->is_troubleshooting() );
+	}
+
+	public function testTroubleshootingModeDisabledWrongCokie() {
 		// Set a troubleshooting cookie with invalid data.
 		$_COOKIE['health-check-disable-plugins'] = 'abc124';
 
 		// This test should fail, as the hash values do not match.
-		$this->assertFalse( $mu_plugin->is_troubleshooting() );
+		$this->assertFalse( $this->class_instance->is_troubleshooting() );
+	}
 
+	public function testTroubleshootingModeEnabledRightCookie() {
 		// Set a troubleshooting cookie with valid data.
 		$_COOKIE['health-check-disable-plugins'] = 'abc123';
 
 		// This test should pass, as the hash values does now match.
-		$this->assertTrue( $mu_plugin->is_troubleshooting() );
-	}
-
-	public function testPushAndPop() {
-		$stack = [];
-		$this->assertEquals( 0, count( $stack ) );
-
-		array_push( $stack, 'foo' );
-		$this->assertEquals( 'foo', $stack[ count( $stack ) - 1 ] );
-		$this->assertEquals( 0, count( $stack ) );
-
-		$this->assertEquals( 'foo', array_pop( $stack ) );
-		$this->assertEquals( 0, count( $stack ) );
+		$this->assertTrue( $this->class_instance->is_troubleshooting() );
 	}
 }
