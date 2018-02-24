14dc9ba2076572e8b63d590e4edd4c81fa07e8ad
Clorith <bg3erl42>
2018-02-24 22:49:09 +0100

    Actually used the aliased entry

--- tests/phpunit/test-mu-plugin.php
+++ tests/phpunit/test-mu-plugin.php
@@ -4,7 +4,7 @@ if ( class_exists( 'PHPUnit\Framework\TestCase' ) ) {
 	class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' );
 }
 
-class AssertionTest extends TestCase {
+class AssertionTest extends PHPUnit_Framework_TestCase {
 
 	public function setUp() {
 		parent::setUp();
