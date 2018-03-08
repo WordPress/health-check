cca560dd384efa8cf0b23658bf36d0c8653e6765
Xenos (xkon) Konstantinos <mr.xkon@gmail.com>
2018-03-08 20:51:55 +0200

    revert back to rows
    revert back to rows

--- src/pages/tools.php
+++ src/pages/tools.php
@@ -11,45 +11,42 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-
-
-	<div class="tool tool-first">
-		<h3><?php esc_html_e( 'File Integrity', 'health-check' ); ?></h3>
-		<div class="tool-content">
+<div class="tool tool-first">
+	<h3><?php esc_html_e( 'File Integrity', 'health-check' ); ?></h3>
+	<div class="tool-content">
+		<p>
+			<?php _e( 'The File Integrity checks all the core files with the <code>checksums</code> provided by the WordPress API to see if they are intact. If there are changes you will be able to make a Diff between the files hosted on WordPress.org and your installation to see what has been changed.', 'health-check' ); ?>
+		</p>
+		<form action="#" id="health-check-file-integrity" method="POST">
 			<p>
-				<?php _e( 'The File Integrity checks all the core files with the <code>checksums</code> provided by the WordPress API to see if they are intact. If there are changes you will be able to make a Diff between the files hosted on WordPress.org and your installation to see what has been changed.', 'health-check' ); ?>
+				<input type="submit" class="button button-primary" value="<?php esc_html_e( 'Check the Files Integrity', 'health-check' ); ?>">
 			</p>
-			<form action="#" id="health-check-file-integrity" method="POST">
-				<p>
-					<input type="submit" class="button button-primary" value="<?php esc_html_e( 'Check the Files Integrity', 'health-check' ); ?>">
-				</p>
-			</form>
-		</div>
-		<div id="tools-file-integrity-response-holder">
-			<span class="spinner"></span>
-		</div>
+		</form>
 	</div>
-	<div class="tool">
-		<h3><?php esc_html_e( 'Mail Check', 'health-check' ); ?></h3>
-		<div class="tool-content">
+	<div id="tools-file-integrity-response-holder">
+		<span class="spinner"></span>
+	</div>
+</div>
+<div class="tool">
+	<h3><?php esc_html_e( 'Mail Check', 'health-check' ); ?></h3>
+	<div class="tool-content">
+		<p>
+			<?php _e( 'The Mail Check will invoke the <code>wp_mail()</code> function and check if it succeeds. We will use the E-mail address you have set up, but you can change it below if you like.', 'health-check' ); ?>
+		</p>
+		<form action="#" id="health-check-mail-check" method="POST">
 			<p>
-				<?php _e( 'The Mail Check will invoke the <code>wp_mail()</code> function and check if it succeeds. We will use the E-mail address you have set up, but you can change it below if you like.', 'health-check' ); ?>
+				<?php
+					$current_user = wp_get_current_user();
+				?>
+				<label for="email"><?php _e( 'E-mail', 'health-check' ); ?></label>
+				<input type="text" name="email" id="email" value="<?php echo $current_user->user_email; ?>">
 			</p>
-			<form action="#" id="health-check-mail-check" method="POST">
-				<p>
-					<?php
-						$current_user = wp_get_current_user();
-					?>
-					<label for="email"><?php _e( 'E-mail', 'health-check' ); ?></label>
-					<input type="text" name="email" id="email" value="<?php echo $current_user->user_email; ?>">
-				</p>
-				<input type="submit" class="button button-primary" value="<?php esc_html_e( 'Check Mail', 'health-check' ); ?>">
-			</form>
-		</div>
-		<div id="tools-mail-check-response-holder">
-			<span class="spinner"></span>
-		</div>
+			<input type="submit" class="button button-primary" value="<?php esc_html_e( 'Check Mail', 'health-check' ); ?>">
+		</form>
 	</div>
-
+	<div id="tools-mail-check-response-holder">
+		<span class="spinner"></span>
+	</div>
+</div>
 <?php
 include_once( HEALTH_CHECK_PLUGIN_DIRECTORY . '/modals/diff.php' );
