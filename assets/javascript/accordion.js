76b8e594f4665d5fd4dd1a64a445e914ffd80e91
Clorith <bg3erl42>
2018-03-18 17:34:15 +0100

    Split up the tools section using (a11y friendly!) accordions, to avoid information overload.

---
+++ assets/javascript/accordion.js
@@ -0,0 +1,21 @@
+jQuery( document ).ready(function( $ ) {
+	$( '.health-check-accordion-trigger' ).click(function() {
+		var isExpanded = ( 'true' === $( this ).attr( 'aria-expanded' ) );
+
+		if ( isExpanded ) {
+			$( this ).attr( 'aria-expanded', 'false' );
+			$( '#' + $( this ).attr( 'aria-controls' ) ).attr( 'hidden', true );
+		} else {
+			$( this ).attr( 'aria-expanded', 'true' );
+			$( '#' + $( this ).attr( 'aria-controls' ) ).attr( 'hidden', false );
+		}
+	});
+
+	$( '.health-check-accordion' ).on( 'keyup', '.health-check-accordion-trigger', function( e ) {
+		if ( '38' === e.keyCode.toString() ) {
+			$( '.health-check-accordion-trigger', $( this ).closest( 'dt' ).prevAll( 'dt' ) ).focus();
+		} else if ( '40' === e.keyCode.toString() ) {
+			$( '.health-check-accordion-trigger', $( this ).closest( 'dt' ).nextAll( 'dt' ) ).focus();
+		}
+	});
+});
