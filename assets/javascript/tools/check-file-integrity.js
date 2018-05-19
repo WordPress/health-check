7b76ac6e8cc9faa799f2e93ab61a59e239aadd78
Clorith <bg3erl42>
2018-05-19 16:18:02 +0200

    Refactor the JavaScript directory slightly, making it a bit more manageable.

--- assets/javascript/tools/check-file-integrity.js
+++ assets/javascript/tools/check-file-integrity.js
@@ -17,33 +17,8 @@ jQuery( document ).ready(function( $ ) {
 				$( '#tools-file-integrity-response-holder .spinner' ).removeClass( 'is-active' );
 				$( '#tools-file-integrity-response-holder' ).parent().css( 'height', 'auto' );
 				$( '#tools-file-integrity-response-holder' ).html( response.data.message );
-			});
-	});
-
-	$( '#health-check-mail-check' ).submit( function( e ) {
-		var email = $( '#health-check-mail-check #email' ).val(),
-			emailMessage = $( '#health-check-mail-check #email_message' ).val(),
-			data;
-
-		e.preventDefault();
-
-		$( '#tools-mail-check-response-holder' ).html( '<span class="spinner"></span>' );
-		$( '#tools-mail-check-response-holder .spinner' ).addClass( 'is-active' );
-
-		data = {
-			'action': 'health-check-mail-check',
-			'email': email,
-			'email_message': emailMessage
-		};
-
-		$.post(
-			ajaxurl,
-			data,
-			function( response ) {
-				$( '#tools-mail-check-response-holder .spinner' ).removeClass( 'is-active' );
-				$( '#tools-mail-check-response-holder' ).parent().css( 'height', 'auto' );
-				$( '#tools-mail-check-response-holder' ).html( response.data.message );
-			});
+			}
+		);
 	});
 
 	$( '#tools-file-integrity-response-holder' ).on( 'click', 'a[href="#health-check-diff"]', function( e ) {
@@ -67,21 +42,7 @@ jQuery( document ).ready(function( $ ) {
 				$( '#health-check-diff-modal #health-check-diff-modal-diff' ).html( response.data.message );
 				$( '#health-check-diff-modal #health-check-diff-modal-content h3' ).html( file );
 				$( '#health-check-diff-modal #health-check-diff-modal-content .spinner' ).removeClass( 'is-active' );
-			});
-	});
-
-	$( '#health-check-diff-modal' ).on( 'click', 'a[href="#health-check-diff-modal-close"]', function( e ) {
-		e.preventDefault();
-		$( '#health-check-diff-modal' ).toggle();
-		$( '#health-check-diff-modal #health-check-diff-modal-diff' ).html( '' );
-		$( '#health-check-diff-modal #health-check-diff-modal-content h3' ).html( '' );
-	});
-
-	$( document ).keyup(function( e ) {
-		if ( 27 === e.which  ) {
-			$( '#health-check-diff-modal' ).css( 'display', 'none' );
-			$( '#health-check-diff-modal #health-check-diff-modal-diff' ).html( '' );
-			$( '#health-check-diff-modal #health-check-diff-modal-content h3' ).html( '' );
-		}
+			}
+		);
 	});
 });
---
+++ assets/javascript/tools/diff-modal.js
@@ -0,0 +1,16 @@
+jQuery( document ).ready(function( $ ) {
+	$( '#health-check-diff-modal' ).on( 'click', 'a[href="#health-check-diff-modal-close"]', function( e ) {
+		e.preventDefault();
+		$( '#health-check-diff-modal' ).toggle();
+		$( '#health-check-diff-modal #health-check-diff-modal-diff' ).html( '' );
+		$( '#health-check-diff-modal #health-check-diff-modal-content h3' ).html( '' );
+	});
+
+	$( document ).keyup(function( e ) {
+		if ( 27 === e.which ) {
+			$( '#health-check-diff-modal' ).css( 'display', 'none' );
+			$( '#health-check-diff-modal #health-check-diff-modal-diff' ).html( '' );
+			$( '#health-check-diff-modal #health-check-diff-modal-content h3' ).html( '' );
+		}
+	});
+});
