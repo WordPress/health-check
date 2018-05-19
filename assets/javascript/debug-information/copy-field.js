ec8ac73c29caa5bae3a053299c0c4f326c6f40a2
Clorith <bg3erl42>
2018-05-19 17:05:12 +0200

    Refactor some more JavaScript

--- assets/javascript/debug-information/copy-field.js
+++ assets/javascript/debug-information/copy-field.js
@@ -14,16 +14,4 @@ jQuery( document ).ready(function( $ ) {
 			$button.text( HealthCheck.string.copied );
 		}
 	});
-
-	$( '.health-check-toc' ).click(function( e ) {
-
-		// Remove the height of the admin bar, and an extra 10px for better positioning.
-		var offset = $( $( this ).attr( 'href' ) ).offset().top - $( '#wpadminbar' ).height() - 10;
-
-		e.preventDefault();
-
-		$( 'html, body' ).animate({
-			scrollTop: offset
-		}, 1200 );
-	});
 });
---
+++ assets/javascript/debug-information/table-of-contents.js
@@ -0,0 +1,14 @@
+/* global HealthCheck */
+jQuery( document ).ready(function( $ ) {
+	$( '.health-check-toc' ).click(function( e ) {
+
+		// Remove the height of the admin bar, and an extra 10px for better positioning.
+		var offset = $( $( this ).attr( 'href' ) ).offset().top - $( '#wpadminbar' ).height() - 10;
+
+		e.preventDefault();
+
+		$( 'html, body' ).animate({
+			scrollTop: offset
+		}, 1200 );
+	});
+});
