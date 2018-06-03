94eb3d9ae4d96aeca713adf09990d19633ec732a
Clorith <bg3erl42>
2018-06-03 23:28:19 +0200

    Introduce primary file integrity tool test

---
+++ tests/phpunit/test-tools.php
@@ -0,0 +1,36 @@
+<?php
+
+class Health_Check_Tools_Test extends WP_UnitTestCase {
+
+	private $test_files_integrity;
+	private $core_checksums;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->test_files_integrity = new Health_Check_Files_Integrity();
+
+		$this->core_checksums = Health_Check_Files_Integrity::call_checksum_api();
+	}
+
+	public function testFilesIntegrityUntampered() {
+		$files = Health_Check_Files_Integrity::parse_checksum_results( $checksums );
+
+		$this->assertEmpty( $files );
+	}
+
+	public function testFilesIntegrityMissingFiles() {
+		$original_file = trailingslashit( ABSPATH ) . 'xmlrpc.php';
+		$renamed_file  = trailingslashit( ABSPATH ) . 'xmlrpc-moved.php';
+
+		rename( $original_file, $renamed_file );
+
+		$files = Health_Check_Files_Integrity::parse_checksum_results( $checksums );
+
+		$this->assertEquals( array(
+			array(
+				$original_file => 'File not found'
+			)
+		), $files );
+	}
+}
