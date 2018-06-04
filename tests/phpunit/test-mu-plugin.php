73ee198fd1d2f17e7b9478154dab9feb573a5c8e
Clorith <bg3erl42>
2018-06-04 02:35:53 +0200

    Test enabling of single plugins.

--- tests/phpunit/test-mu-plugin.php
+++ tests/phpunit/test-mu-plugin.php
@@ -2,10 +2,19 @@
 
 class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 
+	private $test_plugin;
+
 	public function setUp() {
 		parent::setUp();
 
 		$this->class_instance = new Health_Check_Troubleshooting_MU();
+		$this->test_plugin = 'akismet/akismet.php';
+
+		/*
+		 * Start by making sure there are other plugins activated,
+		 * we will use Akismet, as it comes bundled with core.
+		 */
+		activate_plugin( $this->test_plugin, '', false, true );
 
 		// Set up a Troubleshooting hash to test if it is enabled.
 		update_option( 'health-check-disable-plugin-hash', 'abc123' );
@@ -36,20 +45,12 @@ class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 		// Make sure we are not currently troubleshooting.
 		$_COOKIE['health-check-disable-plugins'] = '';
 
-		$test_plugin = 'akismet/akismet.php';
-
-		/*
-		 * Start by making sure there are other plugins activated,
-		 * we will use Akismet, as it comes bundled with core.
-		 */
-		activate_plugin( $test_plugin, '', false, true );
-
 		// Fetch a list of all active plugins.
 		$all_plugins = get_option( 'active_plugins' );
 
 		// Test that the plugin list is what we expect it to be.
 		$this->assertEquals( array(
-			plugin_basename( trim( $test_plugin ) )
+			plugin_basename( trim( $this->test_plugin ) )
 		), $all_plugins );
 
 		// Enable troubleshooting
@@ -60,5 +61,21 @@ class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 
 		// Test taht the plugin list is now empty.
 		$this->assertEmpty( $all_plugins );
+	}
+
+	public function testEnableSinglePlugin() {
+		// Make sure troubleshooting is enabled.
+		$_COOKIE['health-check-disable-plugins'] = 'abc123';
+
+		// Add Akismet to the approved plugins list.
+		update_option( 'health-check-allowed-plugins', array( trim( $this->test_plugin ) ) );
+
+		// Fetch a list of all active plugins.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test that the plugin list is what we expect it to be.
+		$this->assertEquals( array(
+			plugin_basename( trim( $this->test_plugin ) )
+		), $all_plugins );
 	}
 }
