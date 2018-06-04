9a9b303acad9c01c80716a5bb9f95ee54709c9c5
Marius L. Jensen <funman@gmail.com>
2018-06-04 02:55:50 +0200

    Improve unit tests and test framework
    See #97

--- tests/phpunit/test-mu-plugin.php
+++ tests/phpunit/test-mu-plugin.php
@@ -1,15 +1,21 @@
 <?php
 
-if ( class_exists( 'PHPUnit\Framework\TestCase' ) ) {
-	class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' );
-}
+class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 
-class AssertionTest extends PHPUnit_Framework_TestCase {
+	private $test_plugin;
 
-	public function setUp() {
+  public function setUp() {
 		parent::setUp();
 
 		$this->class_instance = new Health_Check_Troubleshooting_MU();
+
+    $this->test_plugin = 'akismet/akismet.php';
+
+		/*
+		 * Start by making sure there are other plugins activated,
+		 * we will use Akismet, as it comes bundled with core.
+		 */
+		activate_plugin( $this->test_plugin, '', false, true );
 
 		// Set up a Troubleshooting hash to test if it is enabled.
 		update_option( 'health-check-disable-plugin-hash', 'abc123' );
@@ -20,7 +26,7 @@ class AssertionTest extends PHPUnit_Framework_TestCase {
 		$this->assertFalse( $this->class_instance->is_troubleshooting() );
 	}
 
-	public function testTroubleshootingModeDisabledWrongCokie() {
+	public function testTroubleshootingModeDisabledWrongCookie() {
 		// Set a troubleshooting cookie with invalid data.
 		$_COOKIE['health-check-disable-plugins'] = 'abc124';
 
@@ -34,5 +40,46 @@ class AssertionTest extends PHPUnit_Framework_TestCase {
 
 		// This test should pass, as the hash values does now match.
 		$this->assertTrue( $this->class_instance->is_troubleshooting() );
+	}
+
+	public function testNoPluginsWhenTroubleshooting() {
+		// Make sure we are not currently troubleshooting.
+		$_COOKIE['health-check-disable-plugins'] = '';
+
+		// Fetch a list of all active plugins.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test that the plugin list is what we expect it to be.
+		$this->assertEquals( array(
+			plugin_basename( trim( $this->test_plugin ) )
+		), $all_plugins );
+
+		// Enable troubleshooting
+		$_COOKIE['health-check-disable-plugins'] = 'abc123';
+
+		// Fetch a list of all active plugins while troubleshooting.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test that the plugin list is now empty.
+		$this->assertEmpty( $all_plugins );
+	}
+
+	public function testEnableSinglePlugin() {
+		// Make sure troubleshooting is enabled.
+		$_COOKIE['health-check-disable-plugins'] = 'abc123';
+
+		// Add Akismet to the approved plugins list.
+		update_option( 'health-check-allowed-plugins', array( 'akismet' ) );
+
+		// Fetch a list of all active plugins while troubleshooting.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test that the plugin list is what we expect it to be.
+		$this->assertEquals( array(
+			plugin_basename( trim( $this->test_plugin ) )
+		), $all_plugins );
+
+		// Empty out the approved plugins list after asserting tests.
+		update_option( 'health-check-allowed-plugins', array() );
 	}
 }
---
+++ tests/phpunit/test-tools.php
@@ -0,0 +1,54 @@
+<?php
+
+class Health_Check_Tools_Test extends WP_UnitTestCase {
+
+	private $test_files_integrity;
+	private $core_checksums;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->test_files_integrity = new Health_Check_Files_Integrity();
+
+		$this->core_checksums = Health_Check_Files_Integrity::call_checksum_api();
+	}
+
+	public function testFilesIntegrityUntampered() {
+		$files = Health_Check_Files_Integrity::parse_checksum_results( $this->core_checksums );
+
+		$this->assertEmpty( $files );
+	}
+
+	public function testFilesIntegrityMissingFiles() {
+		$original_file = trailingslashit( ABSPATH ) . 'xmlrpc.php';
+		$renamed_file  = trailingslashit( ABSPATH ) . 'xmlrpc-moved.php';
+
+		rename( $original_file, $renamed_file );
+
+		$files = Health_Check_Files_Integrity::parse_checksum_results( $this->core_checksums );
+
+		$this->assertEquals( array(
+			array(
+				'xmlrpc.php',
+				'File not found'
+			)
+		), $files );
+
+		rename( $renamed_file, $original_file );
+	}
+
+	public function testFilesIntegiryModifiedFiles() {
+		$filename = trailingslashit( ABSPATH ) . 'xmlrpc.php';
+
+		file_put_contents( $filename, PHP_EOL . '// Modified file content.' . PHP_EOL,  FILE_APPEND );
+
+		$files = Health_Check_Files_Integrity::parse_checksum_results( $this->core_checksums );
+
+		$this->assertEquals( array(
+			array(
+				'xmlrpc.php',
+				'Content changed <a href="#health-check-diff" data-file="xmlrpc.php">(View Diff)</a>'
+			)
+		), $files );
+	}
+}
