2c603bd9f047dd94ed87f50d3228f3a6449fde12
Clorith <bg3erl42>
2018-06-04 02:25:52 +0200

    Test that troubleshooting mode empties out the plugin list when enabled.

--- tests/phpunit/test-mu-plugin.php
+++ tests/phpunit/test-mu-plugin.php
@@ -16,7 +16,7 @@ class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 		$this->assertFalse( $this->class_instance->is_troubleshooting() );
 	}
 
-	public function testTroubleshootingModeDisabledWrongCokie() {
+	public function testTroubleshootingModeDisabledWrongCookie() {
 		// Set a troubleshooting cookie with invalid data.
 		$_COOKIE['health-check-disable-plugins'] = 'abc124';
 
@@ -30,5 +30,35 @@ class Health_Check_MU_Plugin_Test extends WP_UnitTestCase {
 
 		// This test should pass, as the hash values does now match.
 		$this->assertTrue( $this->class_instance->is_troubleshooting() );
+	}
+
+	public function testNoPluginsWhenTroubleshooting() {
+		// Make sure we are not currently troubleshooting.
+		$_COOKIE['health-check-disable-plugins'] = '';
+
+		$test_plugin = 'akismet/akismet.php';
+
+		/*
+		 * Start by making sure there are other plugins activated,
+		 * we will use Akismet, as it comes bundled with core.
+		 */
+		activate_plugin( $test_plugin, '', false, true );
+
+		// Fetch a list of all active plugins.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test that the plugin list is what we expect it to be.
+		$this->assertEquals( array(
+			plugin_basename( trim( $test_plugin ) )
+		), $all_plugins );
+
+		// Enable troubleshooting
+		$_COOKIE['health-check-disable-plugins'] = 'abc123';
+
+		// Fetch a list of all active plugins while troubleshooting.
+		$all_plugins = get_option( 'active_plugins' );
+
+		// Test taht the plugin list is now empty.
+		$this->assertEmpty( $all_plugins );
 	}
 }
