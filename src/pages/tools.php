aa16d3969d0ffecf2fce232dc3bd04864f6abf17
Clorith <bg3erl42>
2018-06-24 14:20:02 +0200

    Add missing doc-block for the tools page, to make it easier to work with.

--- src/pages/tools.php
+++ src/pages/tools.php
@@ -14,6 +14,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <dl id="health-check-tools" role="presentation" class="health-check-accordion">
 	<?php
+	/**
+	 * Filter the features available under the Tools tab.
+	 *
+	 * You may introduce your own, or modify the behavior of existing tools here,
+	 * although we recommend not modifying anything provided by the plugin it self.
+	 *
+	 * Any interactive elements should be introduced using JavaScript and/or CSS, either
+	 * inline, or by enqueueing them via the appropriate actions.
+	 *
+	 * @param array $args {
+	 *     An unassociated array of tabs, listed in the order they are registered.
+	 *
+	 *     @type array $tab {
+	 *         An associated array containing the tab title, and content.
+	 *
+	 *         @type string $label   A pre-escaped string used to label your tool section.
+	 *         @type string $content The content of your tool tab, with any code you may need.
+	 *     }
+	 * }
+	 */
 	$tabs = apply_filters( 'health_check_tools_tab', array() );
 
 	foreach ( $tabs as $count => $tab ) :
