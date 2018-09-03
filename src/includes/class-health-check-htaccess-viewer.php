4d7310fce5d0d563bf48f464812d05890734e524
Jose Castaneda <jomcastaneda@gmail.com>
2018-09-03 12:50:15 -0700

    Adds check for read permission
    This adds a `is_readable()` check for the .htaccess file. If it is
    readable it will continue and output the content.

--- src/includes/class-health-check-htaccess-viewer.php
+++ src/includes/class-health-check-htaccess-viewer.php
@@ -18,38 +18,36 @@ class Health_Check_Htaccess_Viewer {
 	 * @return array Associative array.
 	 */
 	public static function tools_tab( $tabs ) {
-		// check if it is a file.
-		if ( ! is_file( ABSPATH . '.htaccess' ) ) {
-			return $tabs;
+		// check if it is a file and readable.
+		if ( ! is_readable( ABSPATH . '.htaccess' ) ) {
+			$tabs['htaccess-viewer'] = array(
+				'label'   => esc_html__( 'Contents of .htaccess', 'health-check' ),
+				'content' => sprintf( '<div><p>%s</p></div>', esc_html__( 'It appears there is no .htaccess file or is not a readable file.', 'health-check' ) ),
+			);
+		} else {
+			// it is a file.
+			$htaccess_file = file_get_contents( ABSPATH . '/.htaccess' );
+
+			// Remove WordPress rules.
+			$filtered_htaccess_content = trim( preg_replace( '/\# BEGIN WordPress[\s\S]+?# END WordPress/si', '', $htaccess_file ) );
+
+			ob_start(); ?>
+			<div>
+				<p>
+					<button type="button" class="button button-primary health-check-copy-field"><?php esc_html_e( 'Mark field for copying', 'health-check' ); ?></button>
+				</p>
+				<textarea id="htaccess-contents" class="widefat" rows="15"><?php echo htmlspecialchars( file_get_contents( ABSPATH . '.htaccess' ) ); ?></textarea>
+			</div>
+			<?php
+			$content = ob_get_clean();
+
+			$tabs['htacces-viewer'] = array(
+				'label'   => esc_html__( 'Contents of .htaccess', 'health-check' ),
+				'content' => $content,
+			);
 		}
 
-		// It does so let's get the contents
-		$htaccess_file = file_get_contents( ABSPATH . '/.htaccess' );
-
-		// Remove WordPress rules.
-		$filtered_htaccess_content = trim( preg_replace( '/\# BEGIN WordPress[\s\S]+?# END WordPress/si', '', $htaccess_file ) );
-
-		// Return since it is only core added rules.
-		if ( '' === $filtered_htaccess_content ) {
-			return $tabs;
-		}
-
-		ob_start(); ?>
-		<div>
-			<p>
-				<button type="button" class="button button-primary health-check-copy-field"><?php esc_html_e( 'Mark field for copying', 'health-check' ); ?></button>
-			</p>
-			<textarea id="htaccess-contents" class="widefat" rows="15"><?php echo htmlspecialchars( file_get_contents( ABSPATH . '.htaccess' ) ); ?></textarea>
-		</div>
-		<?php
-		$content = ob_get_clean();
-
-		$tabs['htacces-viewer'] = array(
-			'label'   => esc_html__( 'Contents of .htaccess', 'health-check' ),
-			'content' => $content,
-		);
-
-		// return the tabs array since it is a filter.
+		// return our tabs.
 		return $tabs;
 	}
 }
