1939a69e782de215aa8d82452c872f15974835e4
Peter Booker <mail@peterbooker.com>
2018-10-14 23:56:04 +0100

    return of the debug print, blame travis

--- src/includes/class-health-check-updates.php
+++ src/includes/class-health-check-updates.php
@@ -470,6 +470,8 @@ class Health_Check_Updates {
 			return $r; // Not a theme update request.
 		}
 
+		print_r( json_decode( $r['body']['themes'], true )['themes'] );
+
 		$this->themes_before = (array) json_decode( $r['body']['themes'], true );
 
 		return $r;
