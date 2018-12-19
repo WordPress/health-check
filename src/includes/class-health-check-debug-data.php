35d221ce70dfe684aefa4d85d7fae1ad5c542314
Steven Stern <1135810+sterndata@users.noreply.github.com>
2018-12-19 17:53:37 -0600

    wrapped test for php_user in function_exists

--- src/includes/class-health-check-debug-data.php
+++ src/includes/class-health-check-debug-data.php
@@ -397,11 +397,13 @@ class Health_Check_Debug_Data {
 			'label' => __( 'PHP SAPI', 'health-check' ),
 			'value' => ( ! function_exists( 'php_sapi_name' ) ? __( 'Unable to determine PHP SAPI', 'health-check' ) : php_sapi_name() ),
 		);
-		$info['wp-server']['fields'][] = array(
-			'label'   => __( 'PHP User', 'health-check' ),
-			'value'   => get_current_user() . ' (' . getmyuid() . ')',
-			'private' => true,
-		);
+		if ( function_exists( 'get_current_user' ) && function_exists( 'getmyuid' ) {
+			$info['wp-server']['fields'][] = array(
+				'label'   => __( 'PHP User', 'health-check' ),
+				'value'   => get_current_user() . ' (' . getmyuid() . ')',
+				'private' => true,
+			);
+		}
 
 		if ( ! function_exists( 'ini_get' ) ) {
 			$info['wp-server']['fields'][] = array(
