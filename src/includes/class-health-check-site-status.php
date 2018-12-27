9a5fce0c3e81c14a79a6d364db19fbe48a13efd7
Marius Jensen <marius@clorith.net>
2018-12-27 05:25:01 +0100

    Test if the REST API is able to access query parameters

--- src/includes/class-health-check-site-status.php
+++ src/includes/class-health-check-site-status.php
@@ -958,6 +958,11 @@ class Health_Check_Site_Status {
 					wp_remote_retrieve_body( $r )
 				)
 			);
+		} elseif ( false !== ( $json = json_decode( wp_remote_retrieve_body( $r ), true ) ) && ! isset( $json['capabilities'] ) ) {
+			printf(
+				'<span class="warning"></span> %s',
+				esc_html__( 'The REST API did not process the \'context\' query parameter correctly.', 'health-check' )
+			);
 		} else {
 			$json = json_decode( wp_remote_retrieve_body( $r ), true );
 
