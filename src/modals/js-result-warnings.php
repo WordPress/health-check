8fcadf519ae6ee53c798ab246e52a495eb9ae57a
siliconforks <siliconforks@users.noreply.github.com>
2019-01-24 05:09:40 -0400

    Make sure all files are not directly accessible (#241)
    Resolves #237.

--- src/modals/js-result-warnings.php
+++ src/modals/js-result-warnings.php
@@ -1,3 +1,12 @@
+<?php
+
+// Make sure the file is not directly accessible.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'We\'re sorry, but you can not directly access this file.' );
+}
+
+?>
+
 <div class="health-check-modal" data-modal-action="" data-parent-field="">
 	<div class="modal-content">
 		<span class="modal-close">&times;</span>
