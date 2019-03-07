46547190cc2c4594d048111113f38559837f0287
Marius Jensen <marius@clorith.net>
2019-03-07 13:23:29 +0100

    Make the copy to clipboard button first reveal a copy field so that users know what is being copied, and hide the `Copied!` text from screen readers, as it's a notiifcation and we use `wp.a11y.speak` to announce it to screen readers.

--- assets/javascript/debug-information/copy-field.js
+++ assets/javascript/debug-information/copy-field.js
@@ -1,7 +1,7 @@
 /* global HealthCheck, wp */
 jQuery( document ).ready(function( $ ) {
 	$( '.health-check-copy-field' ).click(function( e ) {
-		var $textarea = $( 'system-information-' + $( this ).data( 'copy-field' ) + '-copy-field' ),
+		var $textarea = $( '#system-information-' + $( this ).data( 'copy-field' ) + '-copy-field' ),
 			$wrapper = $( this ).closest( 'div' );
 
 		e.preventDefault();
@@ -9,9 +9,26 @@ jQuery( document ).ready(function( $ ) {
 		$textarea.select();
 
 		if ( document.execCommand( 'copy' ) ) {
-			$( 'copy-field-success', $wrapper ).addClass( 'visible' );
+			$( '.copy-field-success', $wrapper ).addClass( 'visible' );
+			$( this ).focus();
 
 			wp.a11y.speak( HealthCheck.string.site_info_copied, 'polite' );
+		}
+	});
+
+	$( '.health-check-toggle-copy-section' ).click(function( e ) {
+		var $copySection = $( '.system-information-copy-wrapper' );
+
+		e.preventDefault();
+
+		if ( $copySection.hasClass( 'hidden' ) ) {
+			$copySection.removeClass( 'hidden' );
+
+			$( this ).text( HealthCheck.string.site_info_hide_copy );
+		} else {
+			$copySection.addClass( 'hidden' );
+
+			$( this ).text( HealthCheck.string.site_info_show_copy );
 		}
 	});
 });
