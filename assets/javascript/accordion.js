7f8c0dbd175b475325fc6b3da4d2a8d3cabc458b
Marius L. Jensen <funman@gmail.com>
2019-05-06 07:32:21 +0200

    Task/core parity (#334)

--- assets/javascript/accordion.js
+++ assets/javascript/accordion.js
@@ -9,13 +9,5 @@ jQuery( document ).ready(function( $ ) {
 			$( this ).attr( 'aria-expanded', 'true' );
 			$( '#' + $( this ).attr( 'aria-controls' ) ).attr( 'hidden', false );
 		}
-	});
-
-	$( '.health-check-accordion' ).on( 'keyup', '.health-check-accordion-trigger', function( e ) {
-		if ( '38' === e.keyCode.toString() ) {
-			$( '.health-check-accordion-trigger', $( this ).closest( 'dt' ).prevAll( 'dt' ) ).focus();
-		} else if ( '40' === e.keyCode.toString() ) {
-			$( '.health-check-accordion-trigger', $( this ).closest( 'dt' ).nextAll( 'dt' ) ).focus();
-		}
-	});
+	} );
 });
--- assets/javascript/debug-information/copy-field.js
+++ assets/javascript/debug-information/copy-field.js
@@ -1,34 +1,17 @@
-/* global HealthCheck, wp */
-jQuery( document ).ready(function( $ ) {
-	$( '.health-check-copy-field' ).click(function( e ) {
-		var $textarea = $( '#system-information-' + $( this ).data( 'copy-field' ) + '-copy-field' ),
-			$wrapper = $( this ).closest( 'div' );
+/* global ClipboardJS, SiteHealth, wp */
+jQuery( document ).ready( function( $ ) {
+	var clipboard;
 
-		e.preventDefault();
+	if ( 'undefined' !== typeof ClipboardJS ) {
+		clipboard = new ClipboardJS( '.site-health-copy-buttons .copy-button' );
 
-		$textarea.select();
+		// Debug information copy section.
+		clipboard.on( 'success', function( e ) {
+			var $wrapper = $( e.trigger ).closest( 'div' );
 
-		if ( document.execCommand( 'copy' ) ) {
-			$( '.copy-field-success', $wrapper ).addClass( 'visible' );
-			$( this ).focus();
+			$( '.success', $wrapper ).addClass( 'visible' );
 
-			wp.a11y.speak( HealthCheck.string.site_info_copied, 'polite' );
-		}
-	});
-
-	$( '.health-check-toggle-copy-section' ).click(function( e ) {
-		var $copySection = $( '.system-information-copy-wrapper' );
-
-		e.preventDefault();
-
-		if ( $copySection.hasClass( 'hidden' ) ) {
-			$copySection.removeClass( 'hidden' );
-
-			$( this ).text( HealthCheck.string.site_info_hide_copy );
-		} else {
-			$copySection.addClass( 'hidden' );
-
-			$( this ).text( HealthCheck.string.site_info_show_copy );
-		}
-	});
+			wp.a11y.speak( SiteHealth.string.site_info_copied );
+		});
+	}
 });
