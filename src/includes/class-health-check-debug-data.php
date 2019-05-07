5001038a81ff5cf30862b5724e5e0e281f94208a
Marius Jensen <marius@clorith.net>
2019-05-07 17:48:47 +0200

    Add polyfill for improvements to the `recurse_dirsize()` introduced in WordPress 5.2, but required earlier in our plugin so that we can fetch directory sizes when applicable.

--- src/includes/class-health-check-debug-data.php
+++ src/includes/class-health-check-debug-data.php
@@ -1351,11 +1351,10 @@ class Health_Check_Debug_Data {
 			}
 		}
 
-		$handle = opendir( $directory );
-		if ( $handle ) {
+		if ( $handle = opendir( $directory ) ) {
 			while ( ( $file = readdir( $handle ) ) !== false ) {
 				$path = $directory . '/' . $file;
-				if ( '.' != $file && '..' != $file ) {
+				if ( $file != '.' && $file != '..' ) {
 					if ( is_file( $path ) ) {
 						$size += filesize( $path );
 					} elseif ( is_dir( $path ) ) {
