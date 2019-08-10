e47c9719933cf9d1057ede95a22db96872168494
Marius Jensen <marius@clorith.net>
2019-08-10 13:24:46 +0200

    Improve and enable WP-CLI support, allowing to run tests with `wp health-check status`. See #122

--- src/includes/class-health-check-wp-cli.php
+++ src/includes/class-health-check-wp-cli.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Class Health_Check_WP_CLI
  */
-class Health_Check_WP_CLI extends WP_CLI_Command {
+class Health_Check_WP_CLI {
 	/**
 	 * See the sites status based on best practices and WordPress recommendations.
 	 *
@@ -40,29 +40,36 @@ class Health_Check_WP_CLI extends WP_CLI_Command {
 		global $health_check_site_status;
 
 		$all_tests = $health_check_site_status::get_tests();
-		$all_tests = array_merge( $all_tests['direct'], $all_tests['async'] );
 
 		$test_result = array();
 
-		foreach ( $all_tests as $test ) {
-			ob_start();
-			call_user_func( array( $health_check_site_status, 'test_' . $test['test'] ) );
-			$test_output = ob_get_clean();
+		foreach ( $all_tests['direct'] as $test ) {
+			$test_output = call_user_func( $test['test'] );
 
 			$test_result[] = array(
 				'test'   => $test['label'],
-				'result' => wp_kses( $test_output, array() ),
+				'type'   => wp_kses( $test_output['badge']['label'], array() ),
+				'result' => wp_kses( $test_output['status'], array() ),
+			);
+		}
+		foreach ( $all_tests['async'] as $test ) {
+			$test_output = call_user_func( array( $health_check_site_status, 'get_test_' . $test['test'] ) );
+
+			$test_result[] = array(
+				'test'   => $test['label'],
+				'type'   => wp_kses( $test_output['badge']['label'], array() ),
+				'result' => wp_kses( $test_output['status'], array() ),
 			);
 		}
 
 		if ( WP_CLI\Utils\get_flag_value( $assoc_args, 'format' ) === 'json' ) {
-			WP_CLI\Utils\format_items( 'json', $test_result, array( 'test', 'result' ) );
+			WP_CLI\Utils\format_items( 'json', $test_result, array( 'test', 'type', 'result' ) );
 		} elseif ( WP_CLI\Utils\get_flag_value( $assoc_args, 'format' ) === 'csv' ) {
-			WP_CLI\Utils\format_items( 'csv', $test_result, array( 'test', 'result' ) );
+			WP_CLI\Utils\format_items( 'csv', $test_result, array( 'test', 'type', 'result' ) );
 		} elseif ( WP_CLI\Utils\get_flag_value( $assoc_args, 'format' ) === 'yaml' ) {
-			WP_CLI\Utils\format_items( 'yaml', $test_result, array( 'test', 'result' ) );
+			WP_CLI\Utils\format_items( 'yaml', $test_result, array( 'test', 'type', 'result' ) );
 		} else {
-			WP_CLI\Utils\format_items( 'table', $test_result, array( 'test', 'result' ) );
+			WP_CLI\Utils\format_items( 'table', $test_result, array( 'test', 'type', 'result' ) );
 		}
 	}
 }
