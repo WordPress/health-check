c0e8561e868c23ed8d17476d7a6476ac776c90e1
Marius Jensen <marius@clorith.net>
2022-08-27 14:35:40 +0200

    Code structure: Move to a single plugin directory setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,16 @@
 process.env.WP_DEVTOOL = false;
 
 const path = require( 'path' );
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		"clipboard": "clipboard",
-		"react": "react",
-		"react-dom": "react-dom",
-		"health-check": [ path.resolve( process.cwd(), 'src/javascript', 'health-check.js' ), path.resolve( process.cwd(), 'src/sass', 'health-check.scss' ) ],
+		"health-check": [
+			path.resolve( process.cwd(), 'src/javascript', 'health-check.js' ),
+			path.resolve( process.cwd(), 'src/styles', 'health-check.scss' )
+		],
 		"health-check-tools": [ path.resolve( process.cwd(), 'src/javascript', 'tools.js' ) ],
 		"troubleshooting-mode": path.resolve( process.cwd(), 'src/javascript', 'troubleshooting-mode.js' ),
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve( process.cwd(), 'health-check/assets/' ),
-	},
-	plugins: [
-		...defaultConfig.plugins,
-		new CopyPlugin({
-			patterns: [
-				{
-					from: path.resolve( process.cwd(), 'src/php' ),
-					to: path.resolve( process.cwd(), 'health-check' )
-				},
-				{
-					from: path.resolve( process.cwd(), 'docs' ),
-					to: path.resolve( process.cwd(), 'health-check' )
-				}
-			]
-		})
-	],
-	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-		'__' : '@wordpress/i18n'
 	}
 };
