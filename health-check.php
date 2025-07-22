a143a6cfd556c4a7d7d2b39bef43eaee82135d68
anushkaakanojia <anushkaakanojia@gmail.com>
2025-07-22 17:16:44 +0530

    Fix multisite plugin behavior issue

--- health-check.php
+++ health-check.php
@@ -11,7 +11,7 @@
  * Author: The WordPress.org community
  * Author URI: https://wordpress.org/plugins/health-check/
  * Version: 1.7.1
- * Requires PHP: 5.6
+ * Requires PHP: 7.0
  * License: GPLv2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: health-check
