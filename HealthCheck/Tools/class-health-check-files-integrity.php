5f3dbc86de85b7afc7b6cbff1cfec94b4372d4b9
Kaspars Dambis <hi@kaspars.net>
2025-10-16 12:26:11 +0300

    No longer needed since classes don’t call them selves

--- HealthCheck/Tools/class-health-check-files-integrity.php
+++ HealthCheck/Tools/class-health-check-files-integrity.php
@@ -6,11 +6,6 @@
  * @package Health Check
  */
 
-// Make sure the file is not directly accessible.
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'We\'re sorry, but you can not directly access this file.' );
-}
-
 /**
  * Class Files_Integrity
  */
