b7cde4944155ca3098b69eea8bb2c9f24d0f852f
Kaspars Dambis <hi@kaspars.net>
2025-10-16 13:22:42 +0300

    No longer needed
    Reminder: if your plugin or theme is hosted on WordPress.org and is still using load_*_textdomain(), you can remove this call. Since WordPress 4.6, plugins and themes no longer need load_plugin_textdomain() or load_theme_textdomain(). WordPress automatically loads the translations for you when needed. If you still support older WordPress versions or do not host your plugin/theme on WordPress.org, move the function call to a later hook such as init.

--- HealthCheck/class-health-check.php
+++ HealthCheck/class-health-check.php
@@ -50,8 +50,6 @@ class Health_Check {
 	public function init() {
 		add_action( 'init', array( $this, 'init_tools' ), 1 ); // Early to allow tools to register on init as well.
 
-		add_action( 'init', array( $this, 'load_i18n' ) );
-
 		add_filter( 'plugin_action_links', array( $this, 'troubleshoot_plugin_action' ), 20, 4 );
 		add_filter( 'plugin_action_links_' . plugin_basename( HEALTH_CHECK_PLUGIN_FILE ), array( $this, 'page_plugin_action' ) );
 
@@ -216,21 +214,6 @@ class Health_Check {
 		);
 
 		wp_redirect( admin_url( 'plugins.php' ) );
-	}
-
-	/**
-	 * Load translations.
-	 *
-	 * Loads the textdomain needed to get translations for our plugin.
-	 *
-	 * @uses load_plugin_textdomain()
-	 * @uses basename()
-	 * @uses dirname()
-	 *
-	 * @return void
-	 */
-	public function load_i18n() {
-		load_plugin_textdomain( 'health-check', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
